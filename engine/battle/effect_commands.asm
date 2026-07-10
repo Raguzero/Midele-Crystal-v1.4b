11199a3a40c4bf22434670ac03ab0acc20fcd4d7
Raguzero <14874194+Raguzero@users.noreply.github.com>
2026-07-10 16:23:34 +0200

    33% confuse chance of hitting itself (changed from 50%)

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -280,9 +280,9 @@ CheckPlayerTurn:
 	ld de, ANIM_CONFUSED
 	call FarPlayBattleAnimation
 
-	; 50% chance of hitting itself
+	; 33% chance of hitting itself (changed from 50%)
 	call BattleRandom
-	cp 50 percent + 1
+	cp 33 percent + 1
 	jr nc, .not_confused
 
 	; clear confusion-dependent substatus
@@ -530,9 +530,9 @@ CheckEnemyTurn: ; 3421f
 	ld de, ANIM_CONFUSED
 	call FarPlayBattleAnimation
 
-	; 50% chance of hitting itself
+	; 33% chance of hitting itself (changed from 50%)
 	call BattleRandom
-	cp 50 percent + 1
+	cp 33 percent + 1
 	jr nc, .not_confused
 
 	; clear confusion-dependent substatus
