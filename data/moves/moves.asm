38006dbc1dc8cd26ca0f9343349564ddbe3f4e6f
Raguzero <14874194+Raguzero@users.noreply.github.com>
2026-08-24 17:16:07 +0200

    Update THIEF, SUNNY DAY and RAIN DANCE to current gen
    Anyway Sunny Day and Rain Dance always 100% accuracy even if the document says 90% accuracy

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -179,7 +179,7 @@ Moves: ; 41afb
 	move STRUGGLE,     EFFECT_RECOIL_HIT,         50, NORMAL,   100,  1,   0
 	move SKETCH,       EFFECT_SKETCH,              0, NORMAL,   100,  1,   0
 	move TRIPLE_KICK,  EFFECT_TRIPLE_KICK,        10, FIGHTING,  90, 10,   0
-	move THIEF,        EFFECT_THIEF,              60, DARK,     100, 10, 100
+	move THIEF,        EFFECT_THIEF,              60, DARK,     100, 25, 100
 	move SPIDER_WEB,   EFFECT_MEAN_LOOK,           0, BUG,      100, 10,   0
 	move MIND_READER,  EFFECT_LOCK_ON,             0, NORMAL,   100,  5,   0
 	move NIGHTMARE,    EFFECT_NIGHTMARE,           0, GHOST,    100, 15,   0
@@ -251,8 +251,8 @@ Moves: ; 41afb
 	move HIDDEN_POWER, EFFECT_HIDDEN_POWER,       70, CURSE_T,   100, 15,   0
 	move CROSS_CHOP,   EFFECT_NORMAL_HIT,        100, FIGHTING,  80,  5,   0
 	move TWISTER,      EFFECT_TWISTER,            40, DRAGON,   100, 20,  20
-	move RAIN_DANCE,   EFFECT_RAIN_DANCE,          0, WATER,     90,  5,   0
-	move SUNNY_DAY,    EFFECT_SUNNY_DAY,           0, FIRE,      90,  5,   0
+	move RAIN_DANCE,   EFFECT_RAIN_DANCE,          0, WATER,    100,  5,   0
+	move SUNNY_DAY,    EFFECT_SUNNY_DAY,           0, FIRE,     100,  5,   0
 	move CRUNCH,       EFFECT_SP_DEF_DOWN_HIT,    80, DARK,     100, 15,  20
 	move MIRROR_COAT,  EFFECT_MIRROR_COAT,         1, PSYCHIC,  100, 20,   0
 	move PSYCH_UP,     EFFECT_PSYCH_UP,            0, NORMAL,   100, 10,   0
